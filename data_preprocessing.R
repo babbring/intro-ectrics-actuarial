1402cb4aa54ace27679517536350219f2698a042
= <=>
2024-02-02 21:53:23 +0100

    final commit

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -1,33 +1,45 @@
-library(tidyverse)
-library(stargazer)
-setwd("C:/Users/Bram/Desktop/econometrie")
-load("nlsy97.rdata ")
-view(nlsy97)
+library(tidyverse) #importing a popular set of R libraries
+                  #that include some useful functions
+setwd("C:/Users/Bram/Desktop/econometrie")#setting the working directory
+load("nlsy97.rdata ") #loading the data
+view(nlsy97) #this view function give us a full view of 
+             #the dataset in a new window
 
-#creating a vector of all the ranges of white collar job codes
-white_collar_ranges <- c(seq(10, 430), seq(500, 950), seq(1000, 1240), seq(1300, 1530),
-                         seq(1540, 1560), seq(1600, 1760), seq(1800, 1860), seq(1900, 1960),
-                         seq(2000, 2060), seq(2100, 2150), seq(2200, 2340), seq(2400, 2550),
-                         seq(2800, 2960), seq(3000, 3260), seq(3300, 3650), seq(4700, 4960),
-                         seq(5000, 5930))
+#creating a vector of job codes that are in the white collar category
+white_collar_ranges <- c(seq(10, 430), seq(500, 950), seq(1000, 1240),
+                      seq(1300, 1530), seq(1540, 1560), seq(1600, 1760), 
+                      seq(1800, 1860), seq(1900, 1960), seq(2000, 2060),
+                      seq(2100, 2150), seq(2200, 2340), seq(2400, 2550),
+                      seq(2800, 2960), seq(3000, 3260), seq(3300, 3650), 
+                      seq(4700, 4960), seq(5000, 5930))
 
-data <- nlsy97 %>% 
-  subset(wage > 0) %>% 
-  subset(ASVAB > 0) %>% 
-  mutate(ASVAB = ASVAB / 1000) %>% 
-  subset(jobcode > 0 & jobcode < 9950) %>% 
-  subset(exp > 0) %>% 
+data <- nlsy97 %>% # this "%>%" is called a pipe and 
+                   #is part of the tidyverse
+  subset(wage >= 0) %>% #delete all negative values for wage
+  mutate(wage = wage/100) %>%  #scaling the wage
+  subset(ASVAB >= 0) %>% #deleting all negative values for ASVAB
+  mutate(ASVAB = ASVAB / 1000) %>% #scaling the ASVAB score
+  subset(jobcode > 0 & jobcode < 9950) %>% #deleting negative job codes
+                                        #and the special jobs category
+  subset(exp > 0) %>% #deleting all negative values for experience
   subset(hgrade2 >= 0 & hgrade <= 20) %>% 
-  mutate(wcollar = ifelse(jobcode %in% white_collar_ranges, 1, 0))
+  #deleting all invalid values for schooling
+  mutate(wcollar = ifelse(jobcode %in% white_collar_ranges, 1, 0)) %>% 
+  #defining the wcollar dummy variable as 1 for all observations that
+  #have ajobcode in the white_collar_ranges vector, and 0 for the rest
+  subset(mixed != 1) #deleting the mixed race category
 
-nt <- nrow(data)
-print(nt)
+nt <- nrow(data) # assigning the amount of observations to variable nt
+print(nt) #printing nt
 
 #drawing a random sample
-n <- 4000
-rs <- round(1000000000*runif(1), d=0)
-set.seed(rs, kind=NULL)
-print(rs)
-iid <- sample(1:nt,n,replace=FALSE)
-mysample <- data[iid,]
-save(mysample,file="mysample.rdata")
+n <- 4000 #setting the sample size
+rs <- round(1000000000*runif(1), d=0) 
+#drawing a random seed form the uniform distribution
+set.seed(rs, kind=NULL) #setting the random seed
+print(rs) #printing the random seed
+iid <- sample(1:nt,n,replace=FALSE) 
+#defining the indices of the observations that 
+#are included in the sample
+mysample <- data[iid,] #defining the sample
+save(mysample,file="mysample.rdata") #saving the sample locally
--- descriptive_statistics_regressions.R
+++ descriptive_statistics_regressions.R
@@ -1,24 +1,44 @@
-library(stargazer)
-setwd("C:/Users/Bram/Desktop/econometrie")
+library(stargazer) #importing a library that allows exporting
+#the tables to LaTeX
+setwd("C:/Users/Bram/Desktop/econometrie") #setting working directory
 
-load("mysample.rdata")
-mysample <- mysample[, c("white", "male", "black", "hispanic", "mixed", "ASVAB", "hgrade2", "wcollar", "exp", "wage")]
-attach(mysample)
+load("mysample.rdata") #loading data
+mysample <- mysample[, c("ASVAB", "wage", "hgrade2", "wcollar", "exp"
+                         , "male", "white", "black", "hispanic")]
+#including only the variables used in the sample
+attach(mysample) #attaching the sample
 
+#estimating the 3 regression models
 reg1 <- lm(log(wage) ~ 1 + ASVAB)
-reg2 <- lm(log(wage) ~ 1 + ASVAB + hgrade2 + exp + wcollar + male + black + hispanic + mixed)
-reg3 <- lm(log(wage) ~ 1 + ASVAB + hgrade2 + exp + wcollar + male + black + hispanic + mixed+ wcollar*ASVAB + male*ASVAB)
-stargazer(reg1, reg2, reg3, omit.stat=c("LL","ser","f","adj.rsq"), type="text")
-stargazer(as.data.frame(mysample), type="text", nobs=FALSE, min.max=FALSE)
-
-wcollar <- subset(mysample, wcollar==1)
-bcollar <- subset(mysample, wcollar==0)
-male <- subset(mysample, male==1)
-female <- subset(mysample, male==0)
+reg2 <- lm(log(wage) ~ 1 + ASVAB + hgrade2 +
+             wcollar + exp + male + black + hispanic)
+reg3 <- lm(log(wage) ~ 1 + ASVAB + hgrade2 + wcollar + exp +
+             male + black + hispanic + wcollar*ASVAB + male*ASVAB)
 
 
-stargazer(wcollar, type="text", nobs=FALSE, min.max=FALSE)
-stargazer(bcollar, type="text", nobs=FALSE, min.max=FALSE)
-stargazer(subset(mysample, mixed==1), type="text", nobs=FALSE, min.max=FALSE)
+stargazer(reg1, reg2, reg3, omit.stat=c("LL","ser","f","adj.rsq"))
+#making a table for all the regression
 
+stargazer(as.data.frame(mysample), nobs=FALSE,
+          min.max=FALSE, title="Full Sample")
+#making a table for the statistics of the full sample
+
+#making tables for some different categories
+stargazer(as.data.frame(subset(mysample, male==0)),
+          nobs=FALSE, min.max=FALSE, title="Female")
+stargazer(as.data.frame(subset(mysample, male==1)),
+          nobs=FALSE, min.max=FALSE, title="Male")
+stargazer(as.data.frame(subset(mysample, wcollar==0)),
+          nobs=FALSE, min.max=FALSE, title="Blue-collar")
+stargazer(as.data.frame(subset(mysample, wcollar==1))
+          nobs=FALSE, min.max=FALSE, title="White-collar")
+stargazer(as.data.frame(subset(mysample, white==0)),
+          nobs=FALSE, min.max=FALSE, title="White")
+stargazer(as.data.frame(subset(mysample, black==0)),
+          nobs=FALSE, min.max=FALSE, title="Black")
+stargazer(as.data.frame(subset(mysample, hispanic==1)),
+          nobs=FALSE, min.max=FALSE, title"Hispanic")
+
+#calculating the correlation between ASVAB score and the highest 
+#grade completed
 cor(ASVAB, hgrade2)
